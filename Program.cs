a69417ceb2eeaf187c291de76b938e9b5f4f4ab8
ivanPeshterskii <peshterskiivan@gmail.com>
2025-05-22 20:09:35 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿int a = int.Parse(Console.ReadLine());
+int ha = int.Parse(Console.ReadLine()); 
+
+static int Triangle(int a, int ha)
+{
+    return (a * ha) / 2; 
+}
+
+int area = Triangle(a, ha);
+Console.WriteLine(area);
